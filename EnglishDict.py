abca177e2176f108e07b63bdae737d0b20650e54
Jeremiah Lam <jiamialam@gmail.com>
2020-01-13 14:16:31 -0800

    added a spell checking system

--- EnglishDict.py
+++ EnglishDict.py
@@ -1,5 +1,5 @@
 import json
-
+import difflib as dl
 dictionary = json.load(open("data.json"))
 
 #find key was borrowed from a different project and works quite well in this situation. It does not return an error but an empty list if the input is not in the dictionary
@@ -18,17 +18,29 @@ def find_key(target_key, collection):
     return result_list
 
 while True:
-    word = str(input('Please enter word a word to use the dictionary. \nIf you would like to stop please type "/end". \nEnter word: '))
-    if word == '/end':
+    word = str(input('Please enter word a word to use the dictionary. \nIf you would like to stop please type "\end". \nEnter word: '))
+    if word == '\end':
         #this allows the user to end the program.
         break
     else:
         word = word.lower() #removes any non-capital letters
+        while word not in dictionary: #check if misspelled.
+            closest = dl.get_close_matches(word, dictionary.keys(), n = 3)
+            if closest == []:
+                break
+            else:
+                for i in range(len(closest)):
+                    print('Did you mean', closest[i], '?')
+                    answer = input('y/n? ')
+                    if answer == 'y':
+                        word = closest[i]
+                        break
+                break
+        if word not in dictionary: #if not mispelled then restart loop.
+            print('\nThe word does not exist or is not in this dictionary. Please try again.\n')
+            continue
+        
         definition = find_key(word, dictionary)
-        if definition == []:
-            print('\nThis is not a word. Please try again.\n')
-            pass
-        else:
-            definition = definition[0] #find_key always returns a list     
-            for i in range(len(definition)):
-                print('\n', definition.index(definition[i])+1, '. ', definition[i], '\n')
+        definition = definition[0] #find_key always returns a list of items    
+        for i in range(len(definition)):
+            print('\n', definition.index(definition[i])+1, '. ', definition[i], '\n')
