44064e8d78850096273a28a6bbe9a24b2bd1d160
Jeremiah Lam <jiamialam@gmail.com>
2020-01-14 12:45:24 -0800

    adjusted to handle different kinds of punctuations

--- EnglishDictv2.py
+++ EnglishDictv2.py
@@ -1,10 +1,11 @@
 import json
+import re
 from difflib import get_close_matches as gcm
 deflist = json.load(open("data.json"))
 
 def dictionary(word):
     '''takes a word as an input and returns a list of definitions'''
-    word = word.replace('.', "")
+    word = re.sub(r'\W+', "", word)
     wordtypes = [word, word.lower(), word.title(), word.upper()]
     for word in wordtypes: # attempting different iterations of the word
         try:
@@ -15,7 +16,7 @@ def dictionary(word):
         closest = gcm(word, deflist.keys(), cutoff=.8) # attempting 3 words that seem similar at a 80% similarity. User checked.
         for i in range(len(closest)):
             while True:
-                yn = input('Did you mean %s ? y/n: ' % closest[i])
+                yn = input('Did you mean %s? y/n: ' % closest[i])
                 if yn == 'y': #returning correct definition
                     word = closest[i]
                     return dictionary(word)
