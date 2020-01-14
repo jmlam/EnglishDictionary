df7960fd97b964f4050b28bbad20bdd8ccf833e7
Jeremiah Lam <jiamialam@gmail.com>
2020-01-14 11:30:41 -0800

    altered v2 to accomodate for bad user inputs (y/n)

--- EnglishDictv2.py
+++ EnglishDictv2.py
@@ -3,18 +3,24 @@ from difflib import get_close_matches as gcm
 deflist = json.load(open("data.json"))
 
 def dictionary(word):
-    '''takes a word as an input and looks up the definition in a definition list'''
+    '''takes a word as an input and returns a list of definitions'''
     if word in deflist:
         return deflist[word]
     else:
         closest = gcm(word, deflist.keys()) # 60% cutoff
         for i in range(len(closest)):
-            yn = input('Did you mean %s ? y/n: ' % closest[i])
-            if yn == 'y':
-                word = closest[i]
-                return dictionary(word)
+            while True:
+                yn = input('Did you mean %s ? y/n: ' % closest[i])
+                if yn == 'y': #returning correct definition
+                    word = closest[i]
+                    return dictionary(word)
+                elif yn not in 'yn' or yn == "": #checking for valid inputs
+                    print('Please enter a valid input!')
+                    continue
+                else: #moving onto next closest when input is 'n'
+                    break
         
-        return 'This word does not exist. Please double check.'
+        return ['We did not understand your query or this word does not exist. Please double check.']
 
 
 definition = dictionary(input("Please enter a word: "))
