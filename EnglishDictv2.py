83cf26f2fd0910455f2b08a8672ca0eadd181f8d
Jeremiah Lam <jiamialam@gmail.com>
2020-01-14 11:58:15 -0800

    accomodated for different capitalizations

--- EnglishDictv2.py
+++ EnglishDictv2.py
@@ -4,23 +4,25 @@ deflist = json.load(open("data.json"))
 
 def dictionary(word):
     '''takes a word as an input and returns a list of definitions'''
-    if word in deflist:
-        return deflist[word]
-    else:
-        closest = gcm(word, deflist.keys()) # 60% cutoff
-        for i in range(len(closest)):
-            while True:
-                yn = input('Did you mean %s ? y/n: ' % closest[i])
-                if yn == 'y': #returning correct definition
-                    word = closest[i]
-                    return dictionary(word)
-                elif yn not in 'yn' or yn == "": #checking for valid inputs
-                    print('Please enter a valid input!')
-                    continue
-                else: #moving onto next closest when input is 'n'
-                    break
-        
-        return ['We did not understand your query or this word does not exist. Please double check.']
+    for i in [word, word.lower(), word.title()]: # attempting different iterations of the word
+        try:
+            return deflist[i]
+        except:
+            pass
+    closest = gcm(word, deflist.keys()) # 60% cutoff
+    for i in range(len(closest)):
+        while True:
+            yn = input('Did you mean %s ? y/n: ' % closest[i])
+            if yn == 'y': #returning correct definition
+                word = closest[i]
+                return dictionary(word)
+            elif yn not in 'yn' or yn == "": #checking for valid inputs
+                print('Please enter a valid input!')
+                continue
+            else: #moving onto next closest when input is 'n'
+                break
+    print('We did not understand your query or this word does not exist. Please double check.')
+    return ""
 
 
 definition = dictionary(input("Please enter a word: "))
