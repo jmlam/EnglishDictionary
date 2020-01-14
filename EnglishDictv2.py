d9c3065708452d61039f42566a6f80c9414752df
Jeremiah Lam <jiamialam@gmail.com>
2020-01-14 12:28:57 -0800

    tried to optimize a bit better for searching for different word types

--- EnglishDictv2.py
+++ EnglishDictv2.py
@@ -5,23 +5,25 @@ deflist = json.load(open("data.json"))
 def dictionary(word):
     '''takes a word as an input and returns a list of definitions'''
     word = word.replace('.', "")
-    for i in [word, word.lower(), word.title(), word.upper()]: # attempting different iterations of the word
+    wordtypes = [word, word.lower(), word.title(), word.upper()]
+    for word in wordtypes: # attempting different iterations of the word
         try:
-            return deflist[i]
+            return deflist[word]
         except:
             pass
-    closest = gcm(word.lower(), deflist.keys()) # attempting 3 words that seem similar at a 60% similarity. User checked.
-    for i in range(len(closest)):
-        while True:
-            yn = input('Did you mean %s ? y/n: ' % closest[i])
-            if yn == 'y': #returning correct definition
-                word = closest[i]
-                return dictionary(word)
-            elif yn not in 'yn' or yn == "": #checking for valid inputs
-                print('Please enter a valid input!')
-                continue
-            else: #moving onto next closest when input is 'n'
-                break
+    for word in wordtypes: #find closest match                
+        closest = gcm(word, deflist.keys(), cutoff=.8) # attempting 3 words that seem similar at a 80% similarity. User checked.
+        for i in range(len(closest)):
+            while True:
+                yn = input('Did you mean %s ? y/n: ' % closest[i])
+                if yn == 'y': #returning correct definition
+                    word = closest[i]
+                    return dictionary(word)
+                elif yn not in 'yn' or yn == "": #checking for valid inputs
+                    print('Please enter a valid input!')
+                    continue
+                else: #moving onto next closest when input is 'n'
+                    break
     print('We did not understand your query or this word does not exist. Please double check.')
     return ""
 
